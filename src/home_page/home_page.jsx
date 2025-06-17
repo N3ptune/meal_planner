b11cd57ebe4e839378814e00d76c048471a8b98f
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-06-17 17:52:07 -0600

    Move documentation for home_page over, and delete home_page html

--- src/home_page/home_page.jsx
+++ src/home_page/home_page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import 'home_page.css';
 
+// This will be the home page, with meals from individuals that the user has followed, or similar content. One item at a time.
+// Need some way to distinct this from the explore page. Maybe will merge the two.
+
 export function home_page() {
     const something = [];
     
