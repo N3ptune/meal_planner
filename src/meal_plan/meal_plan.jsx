95a455ca0e723ee931e0240878169cd5096362ad
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-06-17 18:05:32 -0600

    Convert meal_plan to jsx

--- src/meal_plan/meal_plan.jsx
+++ src/meal_plan/meal_plan.jsx
@@ -1,2 +1,28 @@
 import React from "react";
 import './meal_plan.css';
+
+{/*
+    
+    This will show the planned meals for the week, or month, or however long I decide to make it.
+    Probably a week because that is pretty far out planning already. It will have a generate/compile
+    grocery list button.
+    
+*/}
+
+export function meal_plan(){
+    return(
+        <>
+            <header>
+                <h1>Meal Plan</h1>
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+                <button onclick = "Location .href = 'grocery_list.html'"/>
+            </footer>
+        </>
+    )
+}
