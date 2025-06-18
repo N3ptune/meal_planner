facee7ab9201a325898092a233a8b0363f526336
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-06-17 18:03:47 -0600

    Convert meal_pinterest to jsx

--- src/meal_pinterest/meal_pinterest.jsx
+++ src/meal_pinterest/meal_pinterest.jsx
@@ -1,2 +1,28 @@
 import React from "react";
 import './meal_pinterest.css'
+
+{/*
+
+    This will be the explore page. It will have lists of meals that are similar to what the user has created, eaten, or liked.
+    It will look more like pinterest, hence the page name. It will have items with names showing, that can be expanded to a meal
+    page. 
+    
+*/}
+
+export function meal_pinterest() {
+    return (
+        <>
+            <header>
+                <h1>More Meals</h1>
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    )
+}
